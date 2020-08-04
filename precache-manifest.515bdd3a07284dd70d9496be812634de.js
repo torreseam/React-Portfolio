0f7d313e8c571c6b465dce9ddca2f2f12359680e
torreseam <615694479+torreseam@users_noreply.github.com>
2020-08-03 21:39:13 -0500

    Updates

---
+++ precache-manifest.515bdd3a07284dd70d9496be812634de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ff2c15ae483a5a53a8bb458a705c46",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6c24f0d038795621a118",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9aca24c89eb6358403d2",
+    "url": "/react-portfolio/static/css/main.bf71aff5.chunk.css"
+  },
+  {
+    "revision": "6c24f0d038795621a118",
+    "url": "/react-portfolio/static/js/2.fb27b891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.fb27b891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aca24c89eb6358403d2",
+    "url": "/react-portfolio/static/js/main.f02dace8.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "62e47b190bfa32d2dfd90ce217446de9",
+    "url": "/react-portfolio/static/media/cover-image-1.62e47b19.jpg"
+  },
+  {
+    "revision": "3f42676ea511a50b6f594bed6fe81025",
+    "url": "/react-portfolio/static/media/github-icon.3f42676e.png"
+  },
+  {
+    "revision": "d8571986f6fd924e3423c67b1ee42db5",
+    "url": "/react-portfolio/static/media/linkedin-circle.d8571986.png"
+  },
+  {
+    "revision": "bfd340280fbd2cbf8351d66d70acbf23",
+    "url": "/react-portfolio/static/media/linkedin.bfd34028.jpg"
+  }
+]);
